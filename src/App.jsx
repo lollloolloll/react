2e5d5badca446391b276b31bea6c042a692b0f98
ganghyeon jo <ganghyeon4479@naver.com>
2024-05-09 10:22:50 +0900

    prettier 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-
-    return (
-        <>
-            <div>
-                <h1>Hello React</h1>
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <h1>Hello React</h1>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
